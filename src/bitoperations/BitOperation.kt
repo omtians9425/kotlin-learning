f7efcc1474c5a6f99e60423deb549aef53b3c971
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-09-17 07:22:38 +0900

    add clear bits operation

--- src/bitoperations/BitOperation.kt
+++ src/bitoperations/BitOperation.kt
@@ -6,6 +6,8 @@ fun main(args: Array<String>) {
     println(getBit(1, 0))
     println(setBit(4, 1))
     println(clearBit(2, 1))
+    println(clearBitsMsbTo0th(13, 3)) //1101
+    println(clearBits0thToIth(13, 2))
 }
 
 //get bit on i th index of num
@@ -18,6 +20,17 @@ fun setBit(num: Int, i: Int): Int {
     return ((1 shl i) or num)
 }
 
+//clear i th bit
 fun clearBit(num: Int, i: Int): Int{
     return (1 shl i).inv() and num
+}
+
+fun clearBitsMsbTo0th(num: Int, i: Int): Int {
+    val mask = (1 shl i) - 1
+    return mask and num
+}
+
+fun clearBits0thToIth(num: Int, i: Int): Int {
+    val mask = (-1 shl (i+1))
+    return mask and num
 }
