6e70d76b1108e0580c51bfbb8dd5ccd0be00f0ed
小俣幸之助 <rskowgshakhtardonetsk32517@gmail.com>
2019-09-17 10:03:38 +0900

    add insert bits method

--- src/bitoperations/BitOperation.kt
+++ src/bitoperations/BitOperation.kt
@@ -7,6 +7,7 @@ fun main(args: Array<String>) {
     println(clearBitsMsbTo0th(13, 3)) //1101
     println(clearBits0thToIth(13, 2))
     println(updateBit(13, 2, false))
+    println(insertBits(107, 7, 1, 4))
 }
 
 //get bit on i th index of num
@@ -34,9 +35,16 @@ fun clearBits0thToIth(num: Int, i: Int): Int {
     return mask and num
 }
 
-//set i th bit for specified bit
 fun updateBit(num: Int, i: Int, bitIs1: Boolean): Int {
     val value = if(bitIs1) 1 else 0
     val mask = (1 shl i).inv()
     return (mask and num) or (value shl i)
+}
+
+//insert m's full bits to n (range i ~ j th)
+fun insertBits(n: Int, m: Int, i: Int, j: Int): Int {
+    val left = -1 shl (j + 1)
+    val right = 1 shl i - 1
+    val clearMask = left or right //mask set 0 for n's i ~j th bits
+    return (n and clearMask) or (m shl i)
 }
