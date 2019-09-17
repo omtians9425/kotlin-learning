905ac092b1e28438794916b4e0572fc6026a6a23
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-09-17 09:11:57 +0900

    implements updateBit

--- src/bitoperations/BitOperation.kt
+++ src/bitoperations/BitOperation.kt
@@ -1,13 +1,12 @@
 package bitoperations
 
-import java.lang.IllegalArgumentException
-
 fun main(args: Array<String>) {
     println(getBit(1, 0))
     println(setBit(4, 1))
     println(clearBit(2, 1))
     println(clearBitsMsbTo0th(13, 3)) //1101
     println(clearBits0thToIth(13, 2))
+    println(updateBit(13, 2, false))
 }
 
 //get bit on i th index of num
@@ -33,4 +32,11 @@ fun clearBitsMsbTo0th(num: Int, i: Int): Int {
 fun clearBits0thToIth(num: Int, i: Int): Int {
     val mask = (-1 shl (i+1))
     return mask and num
+}
+
+//set i th bit for specified bit
+fun updateBit(num: Int, i: Int, bitIs1: Boolean): Int {
+    val value = if(bitIs1) 1 else 0
+    val mask = (1 shl i).inv()
+    return (mask and num) or (value shl i)
 }
