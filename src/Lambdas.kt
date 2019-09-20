b08b2057468e59b5a89a3d8fdd7c518184efdb48
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-09-20 23:34:55 +0900

    add flatten

--- src/Lambdas.kt
+++ src/Lambdas.kt
@@ -3,6 +3,13 @@ fun main() {
     println(list.maxBy { it })
     list = list.map { 2 }.toMutableList()
     print(list.joinToString())
+
+    val listOfList = listOf(
+        listOf(1,2,3),
+        listOf(3,4,5),
+        listOf(5,6,7)
+    )
+    print(listOfList.flatten().toSet())
 }
 
 
