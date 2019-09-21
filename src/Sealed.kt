a2623107183b6c7d659ccfe2312174b2be99cf1b
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-09-21 19:00:33 +0900

    add sealed

---
+++ src/Sealed.kt
@@ -0,0 +1,20 @@
+import Expr.*
+
+fun main() {
+    println(eval(Sum(Sum(Num(5), Num(4)), Num(1))))
+    println(eval(Mul(Sum(Num(5), Num(4)), Num(2))))
+}
+
+sealed class Expr {
+    class Num(val value: Int) : Expr()
+    class Sum(val left: Expr, val right: Expr) : Expr()
+    class Mul(val left: Expr, val right: Expr) : Expr()
+}
+
+//no need to write else
+fun eval(e: Expr): Int =
+    when(e) {
+        is Num -> e.value //smart cast
+        is Sum -> eval(e.left) + eval(e.right)
+        is Mul -> eval(e.left) * eval(e.right)
+    }
