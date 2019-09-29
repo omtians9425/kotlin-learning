b179785a163b83b447ad1cdad535ef76196efb6d
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-09-30 01:21:29 +0900

    implement observable property

--- src/Delegations.kt
+++ src/Delegations.kt
@@ -1,9 +1,11 @@
+import java.beans.PropertyChangeListener
+import java.beans.PropertyChangeSupport
 
 /**
  * Delegation for property's accessor.
  * For interface's implementation, see [Interface.kt]
  */
-fun main () {
+fun main() {
 
     val p = Person("Ken")
     p.emails.forEach { println(it.text) }
@@ -40,3 +42,70 @@ In above, backing property and one time initialization logic is encapsulated
 //        return _emails!!
 //    }
 //}
+
+/**
+ * delegated property sample of change listener
+ */
+open class PropertyChangeHelper {
+    protected val changeSupport = PropertyChangeSupport(this)
+
+    fun addPropertyChangeListener(listener: PropertyChangeListener) {
+        changeSupport.addPropertyChangeListener(listener)
+    }
+
+    fun removePropertyChangeListener(listener: PropertyChangeListener) {
+        changeSupport.removePropertyChangeListener(listener)
+    }
+}
+
+class Person2(
+    val name: String,
+    age: Int,
+    salary: Int
+) : PropertyChangeHelper() {
+
+    var age: Int = age
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+            changeSupport.firePropertyChange(
+                "age", oldValue, newValue
+            )
+        }
+
+    var salary: Int = salary
+        set(newValue) {
+            val oldValue: Int = field
+            field = newValue
+            changeSupport.firePropertyChange(
+                "salary", oldValue, newValue
+            )
+        }
+}
+
+class ObservableProperty(
+    val propName: String,
+    var propValue: Int,
+    val changeSupport: PropertyChangeSupport
+) {
+    fun getValue(): Int = propValue
+    fun setValue(newValue: Int) {
+        val oldValue = propValue
+        propValue = newValue
+        changeSupport.firePropertyChange(propName, oldValue, newValue)
+    }
+}
+
+class Person3(val name: String, age: Int, salary: Int) : PropertyChangeHelper() {
+    val _age = ObservableProperty("age", age, changeSupport)
+    var age: Int
+        get() = _age.getValue()
+        set(value) {
+            _age.setValue(value)
+        }
+
+    val _salary = ObservableProperty("salary", salary, changeSupport)
+    var salary: Int
+    get() = _salary.getValue()
+    set(value) {_salary.setValue(value)}
+}
