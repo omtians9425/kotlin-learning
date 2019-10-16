5a311449fcc0d73b7e87268449acb40bdfbcfe3a
小俣幸之助 <rskow@MacBook-Pro.local>
2019-10-16 22:20:06 +0900

    implement reified type parameter function

--- src/Generics.kt
+++ src/Generics.kt
@@ -1,14 +1,20 @@
 import java.lang.IllegalArgumentException
+import java.util.*
 
 fun main() {
     val list1: List<String> = listOf("a", "b")
     typeCheck(list1)
     typeCheck(1)
 
-    printSum(listOf(1,2,3))
-    printSum(listOf(1.0,2.0,3.0))
-    printSum(setOf(1,2,3)) //Illegal Argument Exception
+    printSum(listOf(1, 2, 3))
+    printSum(listOf(1.0, 2.0, 3.0))
+//    printSum(setOf(1,2,3)) //Illegal Argument Exception
 //    printSum(listOf("1","2","3")) //ClassCastException occurs at sum
+
+    //reified function sample
+    println(listOf(1, 2, "fuga", 3, "hoge").filterInstance<String>())
+    val stringLoader = loadService<String>()
+
 }
 
 fun <T> typeCheck(list: T) {
@@ -18,13 +24,14 @@ fun <T> typeCheck(list: T) {
 //    }
 
     //this can be compiled
-    if(list is List<*>) {
+    if (list is List<*>) {
         println("this is list.")
     } else {
         println("this is not list")
     }
 }
 
+@Suppress("UNCHECKED_CAST")
 fun printSum(c: Collection<*>) {
     /*
     By type erasure, "as" cast only check whether list or not but we can treat as integer.
@@ -32,6 +39,21 @@ fun printSum(c: Collection<*>) {
      */
     val intList = c as? List<Int> ?: throw IllegalArgumentException("List is expected")
     println(intList.sum())
+}
+
+inline fun <reified T> Iterable<*>.filterInstance(): List<T> {
+    val ret = mutableListOf<T>()
+    for (element in this) {
+        //reified type parameter can be use because this method is embedded in the caller with the type resolution
+        if (element is T) {
+            ret.add(element)
+        }
+    }
+    return ret
+}
+
+inline fun <reified T> loadService(): ServiceLoader<T> {
+    return ServiceLoader.load(T::class.java) //use reified parameter
 }
 
 
