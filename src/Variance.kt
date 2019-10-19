1e34409ccfbcd6e0caf42925e56f8cd4d3e5cfd2
小俣幸之助 <rskow@MacBook-Pro.local>
2019-10-19 11:22:04 +0900

    add gitignore

---
+++ src/Variance.kt
@@ -0,0 +1,20 @@
+//fun main() {
+//    val herd = Herd(Cat(), Cat(), Animal())
+//}
+//
+//open class Animal {
+//    fun feed() {
+//        println("feed")
+//    }
+//}
+//
+//class Cat : Animal() {
+//
+//}
+//
+//class Herd<out T : Animal>(private val vararg animals:T) {
+//    val size: Int = animals.size
+//    operator fun get(i: Int): T {
+//        return animals.asList().get(i)
+//    }
+//}
